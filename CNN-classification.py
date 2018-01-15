ce05c8697fe894de7239d4e2a8f6d885f740bdac
Tuyen Luong <tuyenlv17@gmail.com>
2018-01-15 13:21:49 +0700

    add python file

---
+++ CNN-classification.py
@@ -0,0 +1,501 @@
+
+# coding: utf-8
+
+# In[113]:
+
+
+from __future__ import division
+
+
+# In[114]:
+
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as pyplot
+from sklearn.model_selection import train_test_split
+import cv2
+import os
+from skimage import io as ski_io
+from skimage.viewer import ImageViewer
+import time
+from nolearn.lasagne import BatchIterator
+# %matplotlib inline
+
+
+# In[115]:
+
+
+NUM_CLASS=10
+
+
+# In[116]:
+
+
+def add_path_prefix(c):
+    return "0" + str(c)
+def one_hot(labels, n_class = NUM_CLASS):
+    total_record = len(labels)
+    y = np.zeros((total_record, n_class))
+    y[np.arange(total_record), labels] = 1
+    return y
+
+def load_data():
+    X = []
+    y = []
+    path = './signs-gray-32x32'
+    for i in range(10):
+        class_path=path + "/" + add_path_prefix(i)
+        list_files=os.listdir(class_path)
+        for cur_file in list_files:
+            file_path = class_path + "/" + cur_file
+            img = ski_io.imread(file_path, as_grey=True)
+            img = (img / 255.).astype(np.float32)
+#             print (file_path)
+#             print (img)
+#             return 
+            img = img.reshape(img.shape + (1,)) 
+            X.append(img)
+            y.append(i)
+    y = one_hot(np.array(y))
+    return np.array(X, dtype=np.float32), y
+X_train, y_train = load_data()
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.2, random_state=12132)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.1, random_state=54651)
+
+
+# In[117]:
+
+
+from collections import namedtuple
+
+Parameters = namedtuple('Parameters', [
+        # Data parameters
+        'num_classes', 'image_size', 
+        # Training parameters
+        'batch_size', 'max_epochs', 'log_epoch', 'print_epoch',
+        # Optimisations
+        'learning_rate_decay', 'learning_rate',
+        'l2_reg_enabled', 'l2_lambda', 
+        'early_stopping_enabled', 'early_stopping_patience', 
+        'resume_training', 
+        # Layers architecture
+        'conv1_k', 'conv1_d', 'conv1_p', 
+        'conv2_k', 'conv2_d', 'conv2_p', 
+        'conv3_k', 'conv3_d', 'conv3_p', 
+        'fc4_size', 'fc4_p'
+    ])
+
+
+# In[118]:
+
+
+def plot_curve(axis, params, train_column, valid_column, linewidth = 2, train_linestyle = "b-", valid_linestyle = "g-"):
+    """
+    Plots a pair of validation and training curves on a single plot.
+    """
+    model_history = np.load(Paths(params).train_history_path + ".npz")
+    train_values = model_history[train_column]
+    valid_values = model_history[valid_column]
+    epochs = train_values.shape[0]
+    x_axis = np.arange(epochs)
+    axis.plot(x_axis[train_values > 0], train_values[train_values > 0], train_linestyle, linewidth=linewidth, label="train")
+    axis.plot(x_axis[valid_values > 0], valid_values[valid_values > 0], valid_linestyle, linewidth=linewidth, label="valid")
+    return epochs
+
+# Plots history of learning curves for a specific model.
+def plot_learning_curves(params):
+    """
+    Plots learning curves (loss and accuracy on both training and validation sets) for a model identified by a parameters struct.
+    """
+    curves_figure = pyplot.figure(figsize = (10, 4))
+    axis = curves_figure.add_subplot(1, 2, 1)
+    epochs_plotted = plot_curve(axis, parameters, train_column = "train_accuracy_history", valid_column = "valid_accuracy_history")
+
+    pyplot.grid()
+    pyplot.legend()
+    pyplot.xlabel("epoch")
+    pyplot.ylabel("accuracy")
+    pyplot.ylim(50., 115.)
+    pyplot.xlim(0, epochs_plotted)
+
+    axis = curves_figure.add_subplot(1, 2, 2)
+    epochs_plotted = plot_curve(axis, parameters, train_column = "train_loss_history", valid_column = "valid_loss_history")
+
+    pyplot.grid()
+    pyplot.legend()
+    pyplot.xlabel("epoch")
+    pyplot.ylabel("loss")
+    pyplot.ylim(0.0001, 10.)
+    pyplot.xlim(0, epochs_plotted)
+    pyplot.yscale("log")
+
+
+# In[119]:
+
+
+import os
+
+class Paths(object):
+    """
+    Provides easy access to common paths we use for persisting 
+    the data associated with model training.
+    """
+    
+    def __init__(self, params):
+        """
+        Initialises a new `Paths` instance and creates corresponding folders if needed.
+
+        Parameters
+        ----------
+        params  : Parameters
+                  Structure (`namedtuple`) containing model parameters.
+        """
+        self.model_name = self.get_model_name(params)
+        self.var_scope = self.get_variables_scope(params)
+        self.root_path = os.getcwd() + "/models/" + self.model_name + "/"
+        self.model_path = self.get_model_path()
+        self.train_history_path = self.get_train_history_path()
+        self.learning_curves_path = self.get_learning_curves_path()
+        os.makedirs(self.root_path, exist_ok = True)
+
+    def get_model_name(self, params):
+        """
+        Generates a model name with some of the crucial model parameters encoded into the name.
+
+        Parameters
+        ----------
+        params  : Parameters
+                  Structure (`namedtuple`) containing model parameters.
+                  
+        Returns
+        -------
+        Model name.
+        """
+        # We will encode model settings in its name: architecture, optimisations applied, etc.
+        model_name = "k{}d{}p{}_k{}d{}p{}_k{}d{}p{}_fc{}p{}".format(
+            params.conv1_k, params.conv1_d, params.conv1_p, 
+            params.conv2_k, params.conv2_d, params.conv2_p, 
+            params.conv3_k, params.conv3_d, params.conv3_p, 
+            params.fc4_size, params.fc4_p
+        )
+        model_name += "_lrdec" if params.learning_rate_decay else "_no-lrdec"
+        model_name += "_l2" if params.l2_reg_enabled else "_no-l2"
+        return model_name
+
+    def get_variables_scope(self, params):
+        """
+        Generates a model variable scope with some of the crucial model parameters encoded.
+
+        Parameters
+        ----------
+        params  : Parameters
+                  Structure (`namedtuple`) containing model parameters.
+                  
+        Returns
+        -------
+        Variables scope name.
+        """
+        # We will encode model settings in its name: architecture, optimisations applied, etc.
+        var_scope = "k{}d{}_k{}d{}_k{}d{}_fc{}_fc0".format(
+            params.conv1_k, params.conv1_d,
+            params.conv2_k, params.conv2_d,
+            params.conv3_k, params.conv3_d, 
+            params.fc4_size
+        )
+        return var_scope
+
+    def get_model_path(self):
+        """
+        Generates path to the model file.
+   
+        Returns
+        -------
+        Model file path.
+        """
+        return self.root_path + "model.ckpt"
+
+    def get_train_history_path(self):
+        """
+        Generates path to the train history file.
+   
+        Returns
+        -------
+        Train history file path.
+        """
+        return self.root_path + "train_history"
+    
+    def get_learning_curves_path(self):
+        """
+        Generates path to the learning curves graph file.
+   
+        Returns
+        -------
+        Learning curves file path.
+        """
+        return self.root_path + "learning_curves.png"
+
+
+# In[120]:
+
+
+def fully_connected(input, size):
+    """
+    Performs a single fully connected layer pass, e.g. returns `input * weights + bias`.
+    """
+    weights = tf.get_variable( 'weights', 
+        shape = [input.get_shape()[1], size],
+        initializer = tf.contrib.layers.xavier_initializer()
+      )
+    biases = tf.get_variable( 'biases',
+        shape = [size],
+        initializer = tf.constant_initializer(0.0)
+      )
+    return tf.matmul(input, weights) + biases
+
+def fully_connected_relu(input, size):
+    return tf.nn.relu(fully_connected(input, size))
+
+def conv_relu(input, kernel_size, depth):
+    """
+    Performs a single convolution layer pass.
+    """
+    weights = tf.get_variable( 'weights', 
+        shape = [kernel_size, kernel_size, input.get_shape()[3], depth],
+        initializer = tf.contrib.layers.xavier_initializer()
+      )
+    biases = tf.get_variable( 'biases',
+        shape = [depth],
+        initializer = tf.constant_initializer(0.0)
+      )
+    conv = tf.nn.conv2d(input, weights,
+        strides = [1, 1, 1, 1], padding = 'SAME')
+    return tf.nn.relu(conv + biases)
+
+def pool(input, size):
+    """
+    Performs a max pooling layer pass.
+    """
+    return tf.nn.max_pool(
+        input, 
+        ksize = [1, size, size, 1], 
+        strides = [1, size, size, 1], 
+        padding = 'SAME'
+    )
+
+def model_pass(input, params, is_training):
+    # Convolutions
+
+    with tf.variable_scope('conv1'):
+        conv1 = conv_relu(input, kernel_size = params.conv1_k, depth = params.conv1_d) 
+    with tf.variable_scope('pool1'): 
+        pool1 = pool(conv1, size = 2)
+        pool1 = tf.cond(is_training, lambda: tf.nn.dropout(pool1, keep_prob = params.conv1_p), lambda: pool1)
+    with tf.variable_scope('conv2'):
+        conv2 = conv_relu(pool1, kernel_size = params.conv2_k, depth = params.conv2_d)
+    with tf.variable_scope('pool2'):
+        pool2 = pool(conv2, size = 2)
+        pool2 = tf.cond(is_training, lambda: tf.nn.dropout(pool2, keep_prob = params.conv2_p), lambda: pool2)
+    with tf.variable_scope('conv3'):
+        conv3 = conv_relu(pool2, kernel_size = params.conv3_k, depth = params.conv3_d)
+    with tf.variable_scope('pool3'):
+        pool3 = pool(conv3, size = 2)
+        pool3 = tf.cond(is_training, lambda: tf.nn.dropout(pool3, keep_prob = params.conv3_p), lambda: pool3)
+    
+    # Fully connected
+    
+    # 1st stage output
+    pool1 = pool(pool1, size = 4)
+    shape = pool1.get_shape().as_list()
+    pool1 = tf.reshape(pool1, [-1, shape[1] * shape[2] * shape[3]])
+    
+    # 2nd stage output
+    pool2 = pool(pool2, size = 2)
+    shape = pool2.get_shape().as_list()
+    pool2 = tf.reshape(pool2, [-1, shape[1] * shape[2] * shape[3]])    
+    
+    # 3rd stage output
+    shape = pool3.get_shape().as_list()
+    pool3 = tf.reshape(pool3, [-1, shape[1] * shape[2] * shape[3]])
+    
+    flattened = tf.concat([pool1, pool2, pool3], 1)
+    
+    with tf.variable_scope('fc4'):
+        fc4 = fully_connected_relu(flattened, size = params.fc4_size)
+        fc4 = tf.cond(is_training, lambda: tf.nn.dropout(fc4, keep_prob = params.fc4_p), lambda: fc4)
+    with tf.variable_scope('out'):
+        logits = fully_connected(fc4, size = params.num_classes)
+    return logits
+
+
+# In[124]:
+
+
+def train_model(params, X_train, y_train, X_valid, y_valid, X_test, y_test):
+    
+    # Initialisation routines: generate variable scope, create logger, note start time.
+    paths = Paths(params)
+    start = time.time()
+    model_variable_scope = paths.var_scope
+
+    
+    # Build the graph
+    graph = tf.Graph()
+    with graph.as_default():
+        # Input data. For the training data, we use a placeholder that will be fed at run time with a training minibatch.
+        tf_x_batch = tf.placeholder(tf.float32, shape = (None, params.image_size[0], params.image_size[1], 1))
+        tf_y_batch = tf.placeholder(tf.float32, shape = (None, params.num_classes))
+        is_training = tf.placeholder(tf.bool)
+        current_epoch = tf.Variable(0, trainable=False)  # count the number of epochs
+
+        # Model parameters.
+        if params.learning_rate_decay:
+            learning_rate = tf.train.exponential_decay(params.learning_rate, current_epoch, decay_steps = params.max_epochs, decay_rate = 0.01)
+        else:
+            learning_rate = params.learning_rate
+            
+        # Training computation.
+        with tf.variable_scope(model_variable_scope):
+            logits = model_pass(tf_x_batch, params, is_training)
+            if params.l2_reg_enabled:
+                with tf.variable_scope('fc4', reuse = True):
+                    l2_loss = tf.nn.l2_loss(tf.get_variable('weights'))
+            else:
+                l2_loss = 0
+
+        predictions = tf.nn.softmax(logits)
+        softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=tf_y_batch, logits=logits)
+        loss = tf.reduce_mean(softmax_cross_entropy) + params.l2_lambda * l2_loss  
+
+        # Optimizer.
+        optimizer = tf.train.AdamOptimizer(
+            learning_rate = learning_rate
+        ).minimize(loss)
+
+    with tf.Session(graph = graph) as session:
+        session.run(tf.global_variables_initializer())
+
+        # A routine for evaluating current model parameters
+        def get_accuracy_and_loss_in_batches(X, y, is_test=False):
+            p = []
+            sce = []
+            batch_iterator = BatchIterator(batch_size = 128)
+            for x_batch, y_batch in batch_iterator(X, y):
+                [p_batch, sce_batch] = session.run([predictions, softmax_cross_entropy], feed_dict = {
+                        tf_x_batch : x_batch, 
+                        tf_y_batch : y_batch,
+                        is_training : False
+                    }
+                )
+                p.extend(p_batch)
+                sce.extend(sce_batch)                
+            p = np.array(p)
+            sce = np.array(sce)
+            accuracy = 100.0 * np.sum(np.argmax(p, 1) == np.argmax(y, 1)) / p.shape[0]
+            loss = np.mean(sce)
+            if is_test:
+                print ("test accuracy ", accuracy, " test loss ", loss)
+            return (accuracy, loss)
+        
+        # If we chose to keep training previously trained model, restore session.
+        if params.resume_training: 
+            try:
+                tf.train.Saver().restore(session, paths.model_path)
+            except Exception as e:
+                print("Failed restoring previously trained model: file does not exist.")
+                pass
+        
+        saver = tf.train.Saver()
+#         early_stopping = EarlyStopping(tf.train.Saver(), session, patience = params.early_stopping_patience, minimize = True)
+        train_loss_history = np.empty([0], dtype = np.float32)
+        train_accuracy_history = np.empty([0], dtype = np.float32)
+        valid_loss_history = np.empty([0], dtype = np.float32)
+        valid_accuracy_history = np.empty([0], dtype = np.float32)
+        if params.max_epochs > 0:
+            print("================= TRAINING ==================")
+        else:
+            print("================== TESTING ==================")
+        
+        for epoch in range(params.max_epochs):
+            current_epoch = epoch
+            print ("current epoch ", current_epoch)
+            # Train on whole randomised dataset in batches
+            batch_iterator = BatchIterator(batch_size = params.batch_size, shuffle = True)
+            batch_cnt=0
+            for x_batch, y_batch in batch_iterator(X_train, y_train):
+                batch_cnt+=1
+                print ("batch ", batch_cnt)
+                session.run([optimizer], feed_dict = {
+                        tf_x_batch : x_batch, 
+                        tf_y_batch : y_batch,
+                        is_training : True
+                    }
+                )
+
+            # If another significant epoch ended, we log our losses.
+            if (epoch % params.log_epoch == 0):
+                # Get validation data predictions and log validation loss:
+                valid_accuracy, valid_loss = get_accuracy_and_loss_in_batches(X_valid, y_valid)
+
+                # Get training data predictions and log training loss:
+                train_accuracy, train_loss = get_accuracy_and_loss_in_batches(X_train, y_train)
+                print ("train accuracy ", train_accuracy, " train loss ", train_loss)
+                print ("valid accuracy ", valid_accuracy, " valid loss ", valid_loss)
+            else:
+                valid_loss = 0.
+                valid_accuracy = 0.
+                train_loss = 0.
+                train_accuracy = 0.
+                
+            valid_loss_history = np.append(valid_loss_history, [valid_loss])
+            valid_accuracy_history = np.append(valid_accuracy_history, [valid_accuracy])
+            train_loss_history = np.append(train_loss_history, [train_loss])
+            train_accuracy_history = np.append(train_accuracy_history, [train_accuracy])
+            
+
+        # Evaluate on test dataset.
+        test_accuracy, test_loss = get_accuracy_and_loss_in_batches(X_test, y_test)
+#         valid_accuracy, valid_loss = get_accuracy_and_loss_in_batches(X_valid, y_valid)
+        # Save model weights for future use.
+        saved_model_path = saver.save(session, paths.model_path)
+
+        np.savez(paths.train_history_path, train_loss_history = train_loss_history, train_accuracy_history = train_accuracy_history, valid_loss_history = valid_loss_history, valid_accuracy_history = valid_accuracy_history)
+    
+        
+#         plot_learning_curves(params)
+#         pyplot.show()
+
+
+# In[122]:
+
+
+parameters = Parameters(
+    # Data parameters
+    num_classes = NUM_CLASS,
+    image_size = (32, 32),
+    # Training parameters
+    batch_size = 256,
+    max_epochs = 1,
+    log_epoch = 1,
+    print_epoch = 100,
+    # Optimisations
+    learning_rate_decay = False,
+    learning_rate = 0.0001,
+    l2_reg_enabled = True,
+    l2_lambda = 0.0001,
+    early_stopping_enabled = True,
+    early_stopping_patience = 100,
+    resume_training = True,
+    # Layers architecture
+    conv1_k = 5, conv1_d = 32, conv1_p = 0.9,
+    conv2_k = 5, conv2_d = 64, conv2_p = 0.8,
+    conv3_k = 5, conv3_d = 128, conv3_p = 0.7,
+    fc4_size = 1024, fc4_p = 0.5
+)
+
+
+# In[125]:
+
+
+train_model(parameters,X_train, y_train, X_valid, y_valid, X_test, y_test)
+
